1dd4cc53e7b314c1936e895638da0646a32d4803
Demi <gorigoriuhouho0820@gmail.com>
2021-05-04 07:25:24 +0000

    [finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6fcf2092fea5a3b8a0ee147be4285f3089395e5e49df52e717d41dc0e5786ec5b32859588d061e06c7f3d8ee4f89cae6a9f8fd8f877cb78c68806cea70e44060'
